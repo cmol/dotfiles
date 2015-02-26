0cedc22a5b9a02cc35fcb184634b50ff655371e4
Claus Lensbøl <cmol@cmol.dk>
2015-02-26 12:57:11 +0100

    Added bootstrap installer file

---
+++ bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install files and setups
+sudo apt-get install zsh git vim ack-grep exuberant-ctags tmux mosh
+sudo gem install homesick
+
+# Install Janus
+curl -Lo- https://bit.ly/janus-bootstrap | bash
+
+# Install dotfiles
+homesick clone cmol/dotfiles
+homesick link dotfiles
+
+# Install rbenv
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone git://github.com/jf/rbenv-gemset.git ~/.rbenv/plugins/rbenv-gemset
+
+# Change shell to zsh
+chsh -s $(which zsh)
+
+# Things to run locally only
+if [[ -n "$SSH_CLIENT" ]]; then
+  # Create dirs
+  mkdir -p ~/bin
+  cd ~/bin
+
+  # Setup gnome-terminal
+  git clone git@github.com:sigurdga/gnome-terminal-colors-solarized.git
+  cd gnome-terminal-colors-solarized
+  ./set_dark.sh
+
+  # Install poweline fonts
+  mkdir ~/.fonts
+  cd ~/.fonts
+  wget --no-check-certificate https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf -O InconsolataForPowerline.otf
+
+  # List ruby-versions
+  exec $SHELL
+  rbenv install --list
+fi
