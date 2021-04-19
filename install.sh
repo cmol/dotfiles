74beca422b7ed486456e3891cff810db6ae647f1
Claus Lensbøl <cmol@cmol.dk>
2021-04-19 23:01:47 +0200

    Add early installer

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Please open nvim and run \"PlugInstall\""
