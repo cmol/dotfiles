1471324d833888f099b5c50831fb7c2d2c875feb
Claus Lensbøl <cmol@cmol.dk>
2021-04-22 12:43:17 +0000

    Use nvim install path

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 # Neovim
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install neovim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo apt-get install stow neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+stow nvim
 echo "Please open nvim and run \"PlugInstall\""
