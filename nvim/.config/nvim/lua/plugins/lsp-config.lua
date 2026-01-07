49c794d84f49133139705ef45d70e409084a4416
Claus Lensbøl <claus@tailscale.com>
2026-01-07 11:54:23 -0500

    Use telescope builtins for lsp actions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -51,18 +51,27 @@ return {
         },
         on_attach = on_attach,
       })
+      local builtin = require('telescope.builtin')
       vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set("n", "gc", vim.lsp.buf.incoming_calls, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.type_definition, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "gi", builtin.lsp_implementations, {})
+      vim.keymap.set("n", "gc", builtin.lsp_incoming_calls, {})
+      vim.keymap.set("n", "go", builtin.lsp_outgoing_calls, {})
+      vim.keymap.set("n", "gd", builtin.lsp_type_definitions, {})
+      vim.keymap.set("n", "gr", function()
+        builtin.lsp_references(
+          {
+            include_declaration = false, -- Exclude the declaration of the symbol
+            show_line = false,           -- Hide the line content (cleaner view)
+          }
+        )
+      end, {})
       vim.keymap.set("n", "gn", vim.lsp.buf.rename, {})
-      vim.keymap.set("n", "gs", vim.lsp.buf.document_symbol, {})
-      vim.keymap.set("n", "gw", vim.lsp.buf.workspace_symbol, {})
+      vim.keymap.set("n", "gs", builtin.lsp_document_symbols, {})
+      vim.keymap.set("n", "gw", builtin.lsp_dynamic_workspace_symbols, {})
       vim.keymap.set("n", "ga", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, {})
+      vim.keymap.set("n", "<space>q", builtin.diagnostics, {})
       vim.keymap.set("n", "[x", vim.diagnostic.goto_prev, {})
       vim.keymap.set("n", "]x", vim.diagnostic.goto_next, {})
       vim.keymap.set("n", "]s", vim.diagnostic.show, {})
